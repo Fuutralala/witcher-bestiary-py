ff5c7e35087c4c2ea0f13ccf98259e5afcbf2664
Fuu Tralala <fuutralala@outlook.com>
2019-06-28 01:14:27 +0200

    minor fix in basilisk.py

---
+++ cursed.py
@@ -0,0 +1,11 @@
+def Cursed():
+    print("""
+    About which Cursed One would you like to learn more?
+
+    - Archespore
+    - Berserkers
+    - Botchlings
+    - Lubberkin
+    - Ulfhedinn
+    - Werewolves
+    """)
